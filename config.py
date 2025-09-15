4f5723f5616286d4a4e7de13b37c61f2a2c7d40a
qianluxi <qianluxi@sina.com>
2025-09-15 11:02:20 +0800

    实现在HF space里运行，完全云版

---
+++ config.py
@@ -0,0 +1,23 @@
+# config.py
+import os
+
+# 从环境变量获取敏感信息，提供默认值仅用于本地开发
+HF_TOKEN = os.environ.get("HF_TOKEN", "your_hf_token_here")   # 从环境变量获取
+
+MODEL_CONFIG = {
+    'embedding': {
+        'model_id': 'sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2'
+    },
+    'llm': {
+        'repo_id': 'meta-llama/Meta-Llama-3-8B-Instruct',
+        'temperature': 0.7,
+        'max_new_tokens': 512
+    }
+}
+
+ZILLIZ_CONFIG = {
+    "endpoint": os.environ.get("ZILLIZ_ENDPOINT", "https://in03-a43203d37be04a4.serverless.ali-cn-hangzhou.cloud.zilliz.com.cn"),
+    "user": os.environ.get("ZILLIZ_USER", "db_XXX"),
+    "password": os.environ.get("ZILLIZ_PASSWORD", "XXX"),
+    "secure": True
+}
